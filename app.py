9b32578691ea7dc906ab33bb5fbcee8cf2f1eb6a
Hoa <tttennguoidung@gmail.com>
2022-02-19 23:50:06 +0700

    Add simple icon

--- app.py
+++ app.py
@@ -188,5 +188,6 @@ class Canvas(QLabel):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Canvas()
+    window.setWindowIcon(QIcon('icon.png'))
     window.show()
     sys.exit(app.exec_())
